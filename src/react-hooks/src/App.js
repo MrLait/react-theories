8aeec464eca4f5d960d8adb84657698518cf0f6f
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-06 22:56:36 +0300

    react-hooks app init

---
+++ src/react-hooks/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>Add</button>
+      <button>Delete</button>
+    </div>
+  )
+}
+
+export default App;
