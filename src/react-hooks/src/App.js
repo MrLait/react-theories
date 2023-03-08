a47f157ef0ccac8b473128802f0cc03762803a58
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-08 16:22:01 +0300

    useCallback

--- src/react-hooks/src/App.js
+++ src/react-hooks/src/App.js
@@ -1,40 +1,31 @@
-// позволяет кэшировать стейт функции, и соответственно если стейт не поменялся то закэшированная функция не будет вызываться
-// так же позволяет кэшировать объекты т.к после рендеринга создаются новые ссылки на объект, когда меняется стейт, вызывается рендер 
-// и создается новый объект, а useEffect следит за старым объектом видит что он изменился и вызывает функцию заново
-import React, { useState, useMemo, useEffect } from 'react'
+// useCallback решает ту же проблему что и useMemo, но возвращает не результат функции а саму функцию, в [] указываем от чего зависим
+// Нужен что бы функция не изменялась при новом рендере, а кэшировалась
 
-function complexCompute(num) {
-  console.log('complexCompute');
-  let i = 0
-  while (i < 1000000000) { i++ }
-  return num * 2
-}
-
-
+import React, { useCallback, useState } from 'react'
+import ItemsList from './itemsList';
 
 function App() {
-  const [number, setNumber] = useState(42)
   const [colored, setColored] = useState(false);
-  const style = useMemo(() => ({
+  const [count, setCount] = useState(1);
+
+  const styles = {
     color: colored ? 'darkred' : 'black'
-  }), [colored])
+  }
 
-  const computed = useMemo(() => {
-    return complexCompute(number);
-  }, [number])
-
-  useEffect(() => {
-    console.log('Style changed')
-  }, [style])
+  const generateItemsFromApi = useCallback(() => {
+    return new Array(count).fill('').map((_, i) => `Element ${i + 1}`)
+  }, [count])
 
   return (
     <div>
-      <h1 style={style}>Count property:{computed}</h1>
-      <button className={'btn btn-success'} onClick={() => setNumber(prev => prev + 1)}> Add</button>
-      <button className={'btn btn-danger'} onClick={() => setNumber(prev => prev - 1)}> Delete</button>
+      <h1 style={styles}>Count property:{count}</h1>
+      <button className={'btn btn-success'} onClick={() => setCount(prev => prev + 1)}> Add</button>
       <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}> Change</button>
+
+      <ItemsList getItems={generateItemsFromApi} />
     </div>
   )
 }
+
 
 export default App;
