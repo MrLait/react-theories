447e7c83671b65142463254cb9ad0e1c17567eba
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-08 13:44:15 +0300

    useEffect

--- src/react-hooks/src/App.js
+++ src/react-hooks/src/App.js
@@ -1,62 +1,63 @@
-//useState - 
-//Best practice использовать callback функцию которая основана на отслеживании предыдущего состояния
-//т.к useState является асинхронным и просто вызвать setCounter дважды нельзя т.к он выполнится один раз
+//useEffect - служит для отслеживания изменения состояния параметра, на который он подписался и выполнить какую-то логику в ответ на это изменение. 
+// В качестве параметра в useEffect() передается функция. При вызове хука useEffect по сути определяется "эффект", который затем применяется в приложении. 
+// Когда именно применяется? По умолчанию React применяет эффект после каждого рендеринга, в том числе при первом рендеринге приложения. Причем поскольку 
+// подобные эффекты определены внутри компонента, они имеют доступ к объекту props и к состоянию компонента.
 
-//Если проинициализировать useState(initFunction) передав туда функцию, то эта функция будет вызываться дважды
-//перед каждым рендерингом страницы, чтобы оптимизировать этот процесс нужно передать callback с этой функцией
-//которая один раз вычислит начальное значение и не будет туда больше обращаться
+// чтобы указать, что эффект применяется только при изменении переменной name, передадим ее в качестве необязательного параметра в функцию [name]:
+// Если мы хотим, чтобы эффект вызывался только один раз при самом первом рендеринге, то в качестве параметра передаются пустые квадратные скобки - [].
+// Можно эмулировать life cycle hook, чтобы определить момент, когда компонент готов к работе, а так же очищать ресурсы т.е осуществлять подписку/отписку
 
-//Если нужно использовать объекты то для сохранения состояния остальных полей
-// нужно использовать функцию с сохранением в ...prev
-
-import React, { useState } from 'react'
-
-function computeInitialCounter() {
-  console.log("calculation");
-  return Math.trunc(Math.random() * 20)
-}
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  // const [counter, setCounter] = useState(0)
-  // const [counter, setCounter] = useState(computeInitialCounter())
-  const [counter, setCounter] = useState(() => {
-    return computeInitialCounter()
+  const [type, setType] = useState('users');
+  const [data, setData] = useState([])
+  const [pos, setPos] = useState({
+    x: 0, y: 0
   })
+  // useEffect(() => {
+  //   console.log('render');
+  // })
 
-  const [state, setState] = useState({
-    title: 'Counter',
-    date: Date.now()
-  })
+  // useEffect(() => {
+  //   console.log('Type changed : ', { type });
+  // }, [type])
 
-  function increment() {
-    //setCounter(counter + 1)
-    setCounter((prevCounter) => {
-      return prevCounter + 1
-    })
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then(response => response.json())
+      .then(json => setData(json))
 
-    setCounter(prev => prev + 1)
-    setCounter(prev => prev + 1)
-  }
-  function decrement() {
-    setCounter(counter - 1)
-  }
+    return () => {
+      console.log('Clean type');
+    }
+  }, [type])
 
-  function updateTitle() {
-    setState(prev => {
-      return {
-        ...prev,
-        title: "New state"
-      }
+  const mousemoveHandler = event => {
+    setPos({
+      x: event.clientX,
+      y: event.clientY
     })
   }
+
+  useEffect(() => {
+    console.log('ComponentDidMount');
+    // подписка
+    window.addEventListener('mousemove', mousemoveHandler)
+    // отписка
+    return () => {
+      window.removeEventListener('mousemove', mousemoveHandler)
+    }
+  }, [])
+
   return (
     <div>
-      <h1>Counter: {counter} </h1>
-      <button onClick={increment}>Add</button>
-      <button onClick={decrement}>Delete</button>
-      <button onClick={updateTitle}>Change title</button>
-
-      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <h1>Resource: {type}</h1>
+      <button onClick={() => setType('users')}>Users</button>
+      <button onClick={() => setType('todos')}>Todos</button>
+      <button onClick={() => setType('posts')}>Posts</button>
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <pre>{JSON.stringify(pos, null, 2)}</pre>
     </div >
   )
 }
