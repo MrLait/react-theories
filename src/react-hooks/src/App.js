bf2860e9219ed16b2f5ddff5ef6d9c8cf68f0239
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-08 15:50:39 +0300

    useMemo

--- src/react-hooks/src/App.js
+++ src/react-hooks/src/App.js
@@ -1,31 +1,38 @@
-//useRef - позволяет сохранить состояние между рендерингом компонентов и соответственно не вызывает сам рендер
-//Если нам нужно перерисовывать страницу, то нужно использовать хук useState
-// Так же позволяет получать ссылку на дом элементы
-//Позволяет получать значение предыдущего состояния
-import React, { useState, useEffect, useRef } from 'react'
+// позволяет кэшировать стейт функции, и соответственно если стейт не поменялся то закэшированная функция не будет вызываться
+// так же позволяет кэшировать объекты т.к после рендеринга создаются новые ссылки на объект, когда меняется стейт, вызывается рендер 
+// и создается новый объект, а useEffect следит за старым объектом видит что он изменился и вызывает функцию заново
+import React, { useState, useMemo, useEffect } from 'react'
+
+function complexCompute(num) {
+  console.log('complexCompute');
+  let i = 0
+  while (i < 1000000000) { i++ }
+  return num * 2
+}
+
+
 
 function App() {
-  // const [renderCount, setRenderCount] = useState(1)
-  const [value, setValue] = useState('initial')
-  const renderCount = useRef(1)
-  const inputRef = useRef(null)
-  const prevValue = useRef('')
-  useEffect(() => {
-    renderCount.current++;
-    // console.log(inputRef.current.value);
-  })
-  useEffect(() => {
-    prevValue.current = value
-  }, [value])
+  const [number, setNumber] = useState(42)
+  const [colored, setColored] = useState(false);
+  const style = useMemo(() => ({
+    color: colored ? 'darkred' : 'black'
+  }), [colored])
 
-  const focus = () => inputRef.current.focus()
+  const computed = useMemo(() => {
+    return complexCompute(number);
+  }, [number])
+
+  useEffect(() => {
+    console.log('Style changed')
+  }, [style])
+
   return (
     <div>
-      <h1>Number of render: {renderCount.current} </h1>
-      <h2>Prev value: {prevValue.current} </h2>
-
-      <input ref={inputRef} type="text" onChange={e => setValue(e.target.value)} value={value} />
-      <button className='btn btn-success' onClick={focus}>focus</button>
+      <h1 style={style}>Count property:{computed}</h1>
+      <button className={'btn btn-success'} onClick={() => setNumber(prev => prev + 1)}> Add</button>
+      <button className={'btn btn-danger'} onClick={() => setNumber(prev => prev - 1)}> Delete</button>
+      <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}> Change</button>
     </div>
   )
 }
