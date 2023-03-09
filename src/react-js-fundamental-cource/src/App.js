6c70950b3224956f69c8f1566b2f5feea1918b18
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-09 16:43:50 +0300

    function component

--- src/react-js-fundamental-cource/src/App.js
+++ src/react-js-fundamental-cource/src/App.js
@@ -4,29 +4,16 @@
 // в setValue. Это называется двухстороннее связывание. т.е связали состояние со значением в input 
 // подобные компоненты называются управляемыми 
 
+//Создание компонента 
+//Компонент это функция которая возвращает jsx. Компоненты должны именоваться с большой буквы и их нужно экспортировать
+
 import React, { useState } from 'react'
+import Counter from './components/Counter';
 
 function App() {
-  const [likes, setLikes] = useState(0)
-  const [value, setValue] = useState('input text')
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-  function decrement() {
-    setLikes(likes - 1)
-  }
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}> Increment</button>
-      <button onClick={decrement} > Decrement</button>
+      <Counter />
     </div>
   );
 }
