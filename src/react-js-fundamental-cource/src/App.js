ab3d2061283e15b9976c4f75cd2c1615e4e7a46b
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-09 16:14:20 +0300

    react js fundamental init

---
+++ src/react-js-fundamental-cource/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  let likes = 0;
+  function increment() {
+    likes += 1;
+  }
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={() => likes += 1}> Increment</button>
+      <button onClick={() => likes -= 1} > Decrement</button>
+    </div>
+  );
+}
+
+export default App;
