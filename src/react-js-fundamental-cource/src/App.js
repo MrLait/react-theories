9c6265819241c0928dec94c2d1e36dca1e51e77f
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-09 16:35:07 +0300

    managed component and useState

--- src/react-js-fundamental-cource/src/App.js
+++ src/react-js-fundamental-cource/src/App.js
@@ -1,15 +1,32 @@
-import React from 'react'
+//Что бы создать обработчик на кнопку например onClick нужно передать ссылку на функцию и в самой функции изменить состояние
+//Что бы создать управляемый input нужно добавить обработчик onChange, который принимает слушатель event. event принимает первым параметром 
+// event у которого есть dom element - target и у которого есть value - значение которое в нем находится. Соответственно это значение можем помещать 
+// в setValue. Это называется двухстороннее связывание. т.е связали состояние со значением в input 
+// подобные компоненты называются управляемыми 
+
+import React, { useState } from 'react'
 
 function App() {
-  let likes = 0;
+  const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('input text')
+
   function increment() {
-    likes += 1;
+    setLikes(likes + 1)
+  }
+  function decrement() {
+    setLikes(likes - 1)
   }
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <button onClick={() => likes += 1}> Increment</button>
-      <button onClick={() => likes -= 1} > Decrement</button>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+      <button onClick={increment}> Increment</button>
+      <button onClick={decrement} > Decrement</button>
     </div>
   );
 }
