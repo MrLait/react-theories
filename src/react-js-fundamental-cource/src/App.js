55797ee3431d65ebfeebf428f3ffa6bd71930ea0
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-10 00:09:25 +0300

    callback create new post

--- src/react-js-fundamental-cource/src/App.js
+++ src/react-js-fundamental-cource/src/App.js
@@ -17,7 +17,12 @@
 
 //ReactDevTools расширение в хроме
 
+
+//props это некоторые параметры которые может принимать компонент из вне, но обмен этими props-ами всегда идет сверху вниз т.е от родителя
+// к дочернему компоненту, соответственно передача пропсов снизу вверх невозможна. Но из родительского компонента можно передать в дочерний callback
+// функцию обратного вызова. Далее эта функция в дочернем компоненте вызывается и передает данные туда где эта функция была объявлена
 import React, { useState, useRef } from 'react'
+import PostForm from './components/PostForm';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
@@ -30,30 +35,14 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' }
   ])
-  const [post, setPost] = useState({ title: '', body: '' })
 
-  const addNewPost = (e) => {
-    e.preventDefault()
-
-    setPosts([...posts, { ...post, id: Date.now() }])
-    setPost({ title: '', body: '' })
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
   }
+
   return (
     <div className="App">
-      <form>
-        <MyInput
-          value={post.title}
-          onChange={e => setPost({ ...post, title: e.target.value })}
-          type='text'
-          placeholder='post name' />
-        <MyInput
-          value={post.body}
-          onChange={e => setPost({ ...post, body: e.target.value })}
-          type='text'
-          placeholder='post description'
-        />
-        <MyButton onClick={addNewPost}>Create post</MyButton>
-      </form>
+      <PostForm create={createPost} />
       <PostList postList={posts} title="title list" />
     </div >
   );
