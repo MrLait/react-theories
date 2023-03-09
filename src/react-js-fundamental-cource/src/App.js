c4844286d3b2e5cb60d1c82a84ef16fd85735190
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-09 18:21:38 +0300

    css and props

--- src/react-js-fundamental-cource/src/App.js
+++ src/react-js-fundamental-cource/src/App.js
@@ -6,18 +6,21 @@
 
 //Создание компонента 
 //Компонент это функция которая возвращает jsx. Компоненты должны именоваться с большой буквы и их нужно экспортировать
-// классовые компоненты это устаревший подход и лучше использовать функциональные компоненты и хуки
+//классовые компоненты это устаревший подход и лучше использовать функциональные компоненты и хуки
 
+// useState предназначен для управления состояния.
 
 import React, { useState } from 'react'
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
+import PostItem from './components/PostItem';
+import './styles/App.css'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
+      <PostItem post={{ id: 1, title: 'Javascript', body: 'Description' }} />
+      <PostItem post={{ id: 2, title: 'Javascript', body: 'Description' }} />
+      <PostItem post={{ id: 3, title: 'Javascript', body: 'Description' }} />
+      <PostItem post={{ id: 4, title: 'Javascript', body: 'Description' }} />
     </div>
   );
 }
