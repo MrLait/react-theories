e21132b6af4b42505e9fbce4001198cbf3976138
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-09 19:31:57 +0300

    UI component, css modules, props children, prevent button submit

--- src/react-js-fundamental-cource/src/App.js
+++ src/react-js-fundamental-cource/src/App.js
@@ -9,10 +9,14 @@
 //классовые компоненты это устаревший подход и лучше использовать функциональные компоненты и хуки
 
 // useState предназначен для управления состояния.
+//props.children - почитать
+//e.preventDefault() предотвращает обновление страницы при нажатии на кнопку
 
 import React, { useState } from 'react'
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css'
 
 function App() {
@@ -21,9 +25,21 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' }
   ])
-
+  const [title, setTitle] = useState('')
+  const addNewPost = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className="App">
+      <form>
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type='text'
+          placeholder='post name' />
+        <MyInput type='text' placeholder='post description' />
+        <MyButton onClick={addNewPost}>Create post</MyButton>
+      </form>
       <PostList postList={posts} title="title list" />
     </div >
   );
