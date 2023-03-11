f5f3edb264687f7c370051bef19856b874e9cf00
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-03-11 18:34:45 +0300

    Downloadable comments added to the post.

--- src/react-js-fundamental-cource/src/pages/PostIdPage.jsx
+++ src/react-js-fundamental-cource/src/pages/PostIdPage.jsx
@@ -7,19 +7,39 @@ import Loader from '../components/UI/loader/Loader';
 const PostIdPage = () => {
     const params = useParams()
     const [post, setPost] = useState({ id: '', title: '' });
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id)
         setPost(response.data);
     })
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id)
+        setComments(response.data);
+    })
+
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
     }, [])
     return (
         <div>
             <h1> post with id = {params.id} opened</h1>
             {isLoading
                 ? <Loader />
-                : <div > {post.id} {post.title}</div>}
+                : <div > {post.id} {post.title}</div>
+            }
+            <h1> Comments</h1>
+            {isComLoading
+                ? <Loader />
+                : <div>
+                    {comments.map(com =>
+                        <div style={{ marginTop: 15 }}>
+                            <h5>{com.email}</h5>
+                            <div>{com.body}</div>
+                        </div>)}
+                </div>
+
+            }
         </div>
     )
 }
